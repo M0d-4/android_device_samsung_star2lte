d0f8a3ef8c66be38df321ffe20f063f8b67f0c25
Mod4 <omarelfarok24135@gmail.com>
2026-04-21 22:55:07 +0400

    Create device.mk

---
+++ device.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := device/samsung/star2lte
+
+# sideloading
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+# fastbootd
+TW_INCLUDE_FASTBOOTD := true
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libfastboot
