0821fad83d83c79a810e1b49ec570ac6cd7367be
Mod4 <omarelfarok24135@gmail.com>
2026-05-03 12:49:45 +0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,4 +1,6 @@
-LOCAL_PATH := device/samsung/star2lte
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # sideloading
 PRODUCT_PACKAGES += \
@@ -20,6 +22,21 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
     android.hardware.fastboot@1.0-impl-mock \
     android.hardware.fastboot@1.0-impl-mock.recovery
+
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+BOARD_USES_METADATA_PARTITION := true
+
+# Platform
+PLATFORM_VERSION := 99.87.36
+PLATFORM_SECURITY_PATCH := 2127-12-31
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # EROFS utils
 PRODUCT_PACKAGES += \
