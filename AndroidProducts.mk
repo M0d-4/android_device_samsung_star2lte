fa2ac54625f362a11ebe4b2c150838c7be2d058c
Mod4 <omarelfarok24135@gmail.com>
2026-05-03 12:48:20 +0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,1 +1,7 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_star2lte.mk
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_star2lte.mk
+
+COMMON_LUNCH_CHOICES := \
+   omni_star2lte-eng \
+    omni_star2lte-user \
+    omni_star2lte-userdebug
